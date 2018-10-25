42b7a40f9c84e7d75c2fbf3a9aa966032f588cc2
Denis Chernikov <d.chernikov@innopolis.ru>
2018-10-25 03:18:26 +0300

    Interface synchronized; plugs moved to a new file

--- app.py
+++ app.py
@@ -1,143 +1,58 @@
 from flask import Flask, jsonify, request
 
+import request_processing as rp
+
 app = Flask(__name__)
-
-ORDERS = [
-    {
-        'order_id': '1',
-        'order_type': '1',
-        'title': 'Parcel 1',
-        'destination': '127.0,127.0,127.0',
-        'address': 'Universitetskaya 777, d.5, kv.42',
-        'description': 'Regular Letter',
-        'weight': '0.1kg',
-        'dimensions': '9x12x0.1cm',
-        'distance_from_warehouse': '10km',
-        'status': '0',
-        'delivery_state': 'Waiting for Delivery',
-        'customer_name': 'Ivan Ivanov',
-        'customer_phone': '+7(999)0001122',
-        'assigned_to': '-',
-        'delivery_time_from': '12/10/18 14:00',
-        'delivery_time_to': '12/10/18 17:00',
-    },
-    {
-        'order_id': '2',
-        'order_type': '2',
-        'title': 'Parcel 2',
-        'destination': '127.0,127.0,127.0',
-        'address': 'Universitetskaya 676, d.7, kv.42',
-        'description': 'Document Letter',
-        'weight': '2.4kg',
-        'dimensions': '15x20x0.1cm',
-        'distance_from_warehouse': '15km',
-        'status': '0',
-        'delivery_state': 'Waiting for Delivery',
-        'customer_name': 'Ivan Ivanov',
-        'customer_phone': '+7(999)0001122',
-        'assigned_to': '-',
-        'delivery_time_from': '12/10/18 14:00',
-        'delivery_time_to': '12/10/18 17:00',
-    },
-    {
-        'order_id': '3',
-        'order_type': '3',
-        'title': 'Parcel 3',
-        'destination': '127.0,127.0,127.0',
-        'address': 'Kvantovy Bulvar 333, d.99, kv.42',
-        'description': 'Small Parcel',
-        'weight': '2.4kg',
-        'dimensions': '15x20x0.1cm',
-        'distance_from_warehouse': '15km',
-        'status': '0',
-        'delivery_state': 'Waiting for Delivery',
-        'customer_name': 'Ivan Ivanov',
-        'customer_phone': '+7(999)0001122',
-        'assigned_to': 'abc',
-        'delivery_time_from': '12/10/18 14:00',
-        'delivery_time_to': '12/10/18 17:00',
-    },
-    {
-        'order_id': '4',
-        'order_type': '4',
-        'title': 'Parcel 4',
-        'destination': '127.0,127.0,127.0',
-        'address': 'Universitetskaya 676, d.7, kv.42',
-        'description': 'Document Letter',
-        'weight': '2.4kg',
-        'dimensions': '15x20x0.1cm',
-        'distance_from_warehouse': '15km',
-        'status': '0',
-        'delivery_state': 'Waiting for Delivery',
-        'customer_name': 'Ivan Ivanov',
-        'customer_phone': '+7(999)0001122',
-        'assigned_to': 'abc',
-        'delivery_time_from': '12/10/18 14:00',
-        'delivery_time_to': '12/10/18 17:00',
-    },
-]
-
-ASSIGNED_ORDERS = {
-    'abc': ['1', '2'],
-}
-
-
-def are_valid_credentials(login, password):
-    return login == 'abc' and password == '123'
 
 
 @app.route('/login', methods=['POST'])
 def login():
-    if are_valid_credentials(request.json['login'], request.json['password']):
-        return jsonify({'token': 'test_token_successful'})
-    else:
-        return jsonify({'error': 'Invalid auth data'})
+    return jsonify(rp.try_login(request.json))
 
 
 @app.route('/available_orders', methods=['POST'])
-def available_orders():
-    if request.headers['token'] == 'test_token_successful':
-        return jsonify([el for el in ORDERS if el['assigned_to'] == '-'])
-    else:
-        return jsonify({'error': 'Invalid token'})
+def available_orders():  # TODO KeyError
+    return jsonify(rp.get_available_orders(request.headers['token']))
 
 
 @app.route('/assigned_orders', methods=['POST'])
 def assigned_orders():
-    if request.headers['token'] == 'test_token_successful':
-        try:
-            return jsonify([
-                el for el in ORDERS
-                if el['order_id'] in ASSIGNED_ORDERS[request.json['login']]
-            ])
-        except KeyError:
-            return jsonify([])
-    else:
-        return jsonify({'error': 'Invalid token'})
+    return jsonify(rp.get_assigned_orders(request.headers['token']))
 
 
-@app.route('/order/order_details', methods=['POST'])
+@app.route('/order/details', methods=['POST'])
 def order_details():
-    if request.headers['token'] == 'test_token_successful':
-        try:
-            return jsonify(next(
-                el for el in ORDERS
-                if el['order_id'] == request.json['order_id'])
-            )
-        except StopIteration:
-            return jsonify({'error': 'Order does not exist'})
-    else:
-        return jsonify({'error': 'Invalid token'})
+    return jsonify(rp.get_order_details(request.headers['token'], request.json))
 
 
-@app.route('/order/accept_order', methods=['POST'])
-def accept_order():
-    return jsonify({'error': 'Not implemented yet'})  # TODO
+@app.route('/order/accept', methods=['POST'])
+def order_accept():
+    return jsonify(rp.try_order_accept(request.headers['token'], request.json))
 
 
-@app.route('/order/dismiss_order', methods=['POST'])
-def dismiss_order():
-    return jsonify({'error': 'Not implemented yet'})  # TODO
+@app.route('/order/pick', methods=['POST'])
+def order_pick():
+    return jsonify(rp.try_order_pick(request.headers['token'], request.json))
+
+
+@app.route('/order/validate_customer', methods=['POST'])
+def order_validate_customer():
+    return jsonify(rp.try_validate_customer(request.headers['token'], request.json))
+
+
+@app.route('/order/deliver', methods=['POST'])
+def order_deliver():
+    return jsonify(rp.try_order_deliver(request.headers['token'], request.json))
+
+
+@app.route('/order/cancel', methods=['POST'])
+def order_cancel():
+    return jsonify(rp.try_order_cancel(request.headers['token'], request.json))
+
+
+@app.route('/location/update', methods=['POST'])
+def location_update():
+    return jsonify(rp.try_location_update(request.headers['token'], request.json))
 
 
 if __name__ == '__main__':
---
+++ request_processing.py
@@ -0,0 +1,230 @@
+ORDERS = [
+    {
+        'order_id': '1',
+        'title': 'string',
+        'weight': 'string',
+        'dimensions': 'string',
+        'state_code': '1',
+        'state': 'awaiting delivery',
+        'order_type': 'tbd',
+        'warehouse_address': 'string',
+        'recipient_address': 'string',
+        'warehouse_id': 'string',
+        'warehouse_location': '55.753320;48.741012',
+        'recipient_location': '55.817458;49.130425',
+        'delivery_time_from': 'string in some datetime format',
+        'delivery_time_to': 'string in some datetime format',
+        'recipient_phone': 'string in some phone format',
+        'recipient_name': 'string',
+        'assigned_to': None,
+    },
+    {
+        'order_id': '2',
+        'title': 'string',
+        'weight': 'string',
+        'dimensions': 'string',
+        'state_code': '1',
+        'state': 'awaiting delivery',
+        'order_type': 'tbd',
+        'warehouse_address': 'string',
+        'recipient_address': 'string',
+        'warehouse_id': 'string',
+        'warehouse_location': '55.753320;48.741012',
+        'recipient_location': '55.817458;49.130425',
+        'delivery_time_from': 'string in some datetime format',
+        'delivery_time_to': 'string in some datetime format',
+        'recipient_phone': 'string in some phone format',
+        'recipient_name': 'string',
+        'assigned_to': None,
+    },
+    {
+        'order_id': '3',
+        'title': 'string',
+        'weight': 'string',
+        'dimensions': 'string',
+        'state_code': '1',
+        'state': 'awaiting delivery',
+        'order_type': 'tbd',
+        'warehouse_address': 'string',
+        'recipient_address': 'string',
+        'warehouse_id': 'string',
+        'warehouse_location': '55.753320;48.741012',
+        'recipient_location': '55.817458;49.130425',
+        'delivery_time_from': 'string in some datetime format',
+        'delivery_time_to': 'string in some datetime format',
+        'recipient_phone': 'string in some phone format',
+        'recipient_name': 'string',
+        'assigned_to': 'abc',
+    },
+    {
+        'order_id': '4',
+        'title': 'string',
+        'weight': 'string',
+        'dimensions': 'string',
+        'state_code': '1',
+        'state': 'awaiting delivery',
+        'order_type': 'tbd',
+        'warehouse_address': 'string',
+        'recipient_address': 'string',
+        'warehouse_id': 'string',
+        'warehouse_location': '55.753320;48.741012',
+        'recipient_location': '55.817458;49.130425',
+        'delivery_time_from': 'string in some datetime format',
+        'delivery_time_to': 'string in some datetime format',
+        'recipient_phone': 'string in some phone format',
+        'recipient_name': 'string',
+        'assigned_to': 'abc',
+    },
+]
+
+
+def _err_dict(label):
+    return {'error': label}
+
+
+def _are_valid_credentials(login, password_hash):
+    """
+    Ask data cluster if given authentication pair is correct
+    and belongs to the correct user.
+    :param login: Login of user that tries to log in
+    :param password_hash: User password's hash
+    :return: `True' - there is a user with given credentials
+    """
+    return login == 'abc' and password_hash == '123'  # TODO actual request
+
+
+def _generate_token(login):
+    return 'new_access_token'  # TODO
+
+
+def _is_valid_token(token):
+    return token == 'new_access_token'  # TODO
+
+
+def _get_login_by_token(token):
+    return 'abc'  # TODO
+
+
+def try_login(data):
+    """
+    Try to login with given credentials or give an error response.
+    :param data: Dictionary with `login` and `password_hash`
+    :return: Dictionary with `token` of authenticated user or `error`
+    """
+    try:
+        login = data['login']
+        password_hash = data['password_hash']
+    except KeyError:
+        return _err_dict('Required field(s) missed')
+    if _are_valid_credentials(login, password_hash):
+        return {'token': _generate_token(login)}
+    else:
+        return _err_dict('Invalid authentication data')
+
+
+def get_available_orders(token):
+    if not _is_valid_token(token):
+        return _err_dict('Invalid token')
+    return [el for el in ORDERS if not el['assigned_to']]  # TODO
+
+
+def get_assigned_orders(token):
+    if not _is_valid_token(token):
+        return _err_dict('Invalid token')
+    login = _get_login_by_token(token)
+    return [el for el in ORDERS if el['assigned_to'] == login]  # TODO
+
+
+def get_order_details(token, data):
+    if not _is_valid_token(token):
+        return _err_dict('Invalid token')
+    try:
+        order_id = data['order_id']
+    except KeyError:
+        return _err_dict('Required field missed')
+    try:  # TODO
+        return next(
+            el for el in ORDERS
+            if el['order_id'] == order_id
+        )
+    except StopIteration:
+        return _err_dict('Order with given ID does not exist')
+
+
+def try_order_accept(token, data):
+    if not _is_valid_token(token):
+        return _err_dict('Invalid token')
+    try:
+        order_id = data['order_id']
+    except KeyError:
+        return _err_dict('Required field missed')
+    i = 0  # TODO
+    while i < len(ORDERS):
+        if ORDERS[i]['order_id'] == order_id:
+            ORDERS[i]['assigned_to'] = _get_login_by_token(token)
+            break
+    if i == len(ORDERS):
+        return _err_dict('Order with given ID does not exist')
+    return {}  # TODO
+
+
+def try_order_pick(token, data):
+    if not _is_valid_token(token):
+        return _err_dict('Invalid token')
+    try:
+        order_id = data['order_id']
+        key = data['key']
+    except KeyError:
+        return _err_dict('Required field(s) missed')
+    if False:  # TODO
+        return _err_dict('Order picking failed')
+    return {}  # TODO
+
+
+def try_validate_customer(token, data):
+    if not _is_valid_token(token):
+        return _err_dict('Invalid token')
+    try:
+        order_id = data['order_id']
+    except KeyError:
+        return _err_dict('Required field missed')
+    if False:  # TODO
+        return _err_dict('Customer validation failed')
+    return {}  # TODO
+
+
+def try_order_deliver(token, data):
+    if not _is_valid_token(token):
+        return _err_dict('Invalid token')
+    try:
+        order_id = data['order_id']
+        key = data['key']
+    except KeyError:
+        return _err_dict('Required field(s) missed')
+    if False:  # TODO
+        return _err_dict('Order deliver try failed')
+    return {}  # TODO
+
+
+def try_order_cancel(token, data):
+    if not _is_valid_token(token):
+        return _err_dict('Invalid token')
+    try:
+        order_id = data['order_id']
+    except KeyError:
+        return _err_dict('Required field missed')
+    if False:  # TODO
+        return _err_dict('Order cancellation failed')
+    return {}  # TODO
+
+
+def try_location_update(token, data):
+    if not _is_valid_token(token):
+        return _err_dict('Invalid token')
+    try:
+        location = data['location']
+    except KeyError:
+        return _err_dict('Required field missed')
+    if False:  # TODO
+        return _err_dict('Location update failed')
+    return {}  # TODO
